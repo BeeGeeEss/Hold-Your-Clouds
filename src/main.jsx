e3a6582d2b425263483df98cfdaeafbd131ba5f6
BeeGeeEss <brando.g.smith@gmail.com>
2026-07-19 13:01:23 +1000

    CHORE: Clean up

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import "./index.css";
+import "./App.css";
 import "./styles/markdown.css";
 
 import { ThemeProvider, CssBaseline } from "@mui/material";
